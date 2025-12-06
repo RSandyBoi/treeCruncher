5c0753196d67ecc3ff910895b4436dd168cfda6b
RSandyBoi <Ryans121@outlook.com>
2025-12-05 21:28:44 -0800

    completed tree object initialization

--- treeCruncher.py
+++ treeCruncher.py
@@ -1,14 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 class Tree:
-    def __init__(self, ID, species_code, height, DBH, health_status):
+    def __init__(self, ID, species_code, height, DBH1, DBH2, health_status):
         self.ID = ID
         self.species_code = species_code
         self.height = height
-        self.DBH = DBH
+        self.DBH1 = DBH1
+        self.DBH2 = DBH2
         self.health_status = health_status
+        self.delta = abs(DBH1 - DBH2)
+
+    def get_id(self):
+        return self.ID
+    def get_species_code(self):
+        return self.species_code
+    def get_height(self):
+        return self.height
+    def get_DBH(self):
+        return self.DBH1
+    def get_health_status(self):
+        return self.health_status
 
     def plot(self):
         plt.hist(self.data, bins=30, alpha=0.7, color='blue')
