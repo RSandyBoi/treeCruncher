47fdc086b28b114f37cbde027ec9b90079006a88
RSandyBoi <Ryans121@outlook.com>
2025-12-05 21:13:58 -0800

    initial file setup, create initialized tree class

---
+++ treeCruncher.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+class Tree_oneStem:
+    def __init__(self, ID, species_code, height, DBH, health_status):
+        self.ID = ID
+        self.species_code = species_code
+        self.height = height
+        self.DBH = DBH
+        self.health_status = health_status
+
+    def crunch(self):
+        # Example crunching: calculate mean and standard deviation of the data
+        #self.mean = np.mean(self.data)
+        #self.std = np.std(self.data)
+        #return self.mean, self.std
+
+    def plot(self):
+        plt.hist(self.data, bins=30, alpha=0.7, color='blue')
+        plt.axvline(self.mean, color='red', linestyle='dashed', linewidth=1)
+        plt.title('Data Distribution')
+        plt.xlabel('Value')
+        plt.ylabel('Frequency')
+        plt.show()
