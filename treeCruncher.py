f80d9cba690eeb9f4142a703d16fce4ae9c6b270
RSandyBoi <Ryans121@outlook.com>
2025-12-09 23:21:36 -0800

    key functionality established, method to access rest of dataset now established

--- treeCruncher.py
+++ treeCruncher.py
@@ -1,9 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import treeChar as tc
 
 def main():
     print("Tree Cruncher v1.0")
+    dataset = pd.read_excel("DBHValidation_11-10-2025_11-11-2025.xlsx", sheet_name="Data (Same Num Stems)")  # Load dataset from Excel file
+    print("Dataset loaded successfully.")
+    row = dataset.iloc[0]  # Access the first row of the dataset
+    print(row.iloc[0])
 
-if __name__ == "__main__":
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__": #ensures that main() runs only when executed directly
     main()
